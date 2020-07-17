dc09d65d3eebce7b36895aa839f702b39e7b6d4d
mateus <mateushbsb@gmail.com>
2020-07-16 22:02:47 -0300

    Tratative erros

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -3,6 +3,8 @@ import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 
+import AppError from '../erros/AppError';
+
 import uploadConfig from '../config/upload';
 import User from '../models/User';
 
@@ -18,7 +20,10 @@ class UpdateUserAvatarService {
         const user = await usersRepository.findOne(user_id);
 
         if (!user) {
-            throw new Error('Only authenticated users con change avatar');
+            throw new AppError(
+                'Only authenticated users con change avatar',
+                401,
+            );
         }
 
         if (user.avatar) {
